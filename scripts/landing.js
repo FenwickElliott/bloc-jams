e0e9d17e33c4a5f1dc6ffac50ac26cb62c8778a7
Charles <Charles@FenwickElliott.io>
2017-01-10 15:55:05 -0500

    cp-16 completed: landing.js completely refactored

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,26 +1,22 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(p) {
-
-	var revealPoint = function(p){
-		p.style.opacity = 1;
-		p.style.transform = "scaleX(1) translateY(0)";
-		p.style.msTransform = "scaleX(1) translateY(0)";
-		p.style.WebkitTransform = "scaleX(1) translate(0)";
+var animatePoints = function (){
+	var revealPoint = function(){
+		$(this).css({
+			opacity: 1,
+			transform: 'scaleX(1) translateY(0)'
+		});
 	};
-	forEach(p, revealPoint)
+	$.each($('.point'), revealPoint);
 };
 
 $(window).ready(function(){
 	if ($(window).height() > 950){
-			animatePoints(pointsArray);
+			animatePoints();
 		}
 	var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
 	$(window).scroll(function(event){
 		if ($(window).scrollTop() >= scrollDistance){
-			animatePoints(pointsArray);
+			animatePoints();
 		}
 	});
-
 });
